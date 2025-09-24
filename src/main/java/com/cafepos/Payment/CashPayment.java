0e3b4fc5d865816dbfb62d8fb8cf4e31981dfdde
Stephen <93441585+Spuddytime@users.noreply.github.com>
2025-09-24 10:35:10 +0100

    Lab 3 Starting Scaffolding

---
+++ src/main/java/com/cafepos/Payment/CashPayment.java
@@ -0,0 +1,7 @@
+package com.cafepos.Payment;
+
+// class Should not modify the order total it should only handle HOW the payment is made
+
+public final class CashPayment  {
+
+}
---
+++ src/main/java/com/cafepos/Payment/PaymentStrategy.java
@@ -0,0 +1,9 @@
+package com.cafepos.Payment;
+
+import com.cafepos.domain.Order;
+
+    public interface  PaymentStrategy {
+
+        void pay(Order order);
+    }
+
